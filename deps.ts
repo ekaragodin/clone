1bcb8e2b4b99e41d7310221e2a38aacd13a9a2f6
Evgenii Karagodin <ekaragodin@gmail.com>
2019-07-16 10:37:23 +0700

    Fix versions for dependencies

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-import * as fs from "https://deno.land/std/fs/mod.ts";
-import * as stdPath from "https://deno.land/std/fs/path.ts";
-import * as extraPath from "https://denopkg.com/ekaragodin/deno_extra/path/mod.ts";
+import * as fs from "https://deno.land/std@v0.11.0/fs/mod.ts";
+import * as stdPath from "https://deno.land/std@v0.11.0/fs/path.ts";
+import * as extraPath from "https://denopkg.com/ekaragodin/deno_extra@v1.0.0/path/mod.ts";
 
 const path = {
   ...stdPath,
